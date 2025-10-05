e25a3a087c561ab7c285eb34b549195f6dd144cc
Robert Gruevski <robertgruevski@icloud.com>
2025-10-05 17:29:30 -0400

    Initial Comit

---
+++ MacOSNotesApp/MacOSNotesApp/MacOSNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSNotesAppApp.swift
+//  MacOSNotesApp
+//
+//  Created by Robert Gruevski on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MacOSNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
